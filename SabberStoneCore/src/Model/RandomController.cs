c8850fae47c88d0c0f10ba2939563c43b1b7d26c
Marc Laval <marc.laval.opensource@gmail.com>
2018-02-16 18:37:54 +0100

    chore: rebase to 2.0.0

--- SabberStoneCore/src/Model/RandomController.cs
+++ SabberStoneCore/src/Model/RandomController.cs
@@ -22,7 +22,7 @@ namespace SabberStoneCore.Model
 		Card PickPotionSpell(IEntity source, IEntity target, List<Card> cards);
 		Minion PickRecruit(IEntity source, IEntity target, List<Minion> minions);
 		Card PickReplace(IZone zone, IEntity source, IEntity target, List<Card> cards);
-        Card PickSpell(IEntity source, IEntity target, Card[] card);
+        Card PickSpell(IEntity source, IEntity target, Card[] cards);
 		IPlayable PickTarget(EntityType type, IEntity source, IEntity target, List<IPlayable> entities);
 		Card PickTransformMinion(IEntity source, IEntity target, List<Card> cards);
 
@@ -43,11 +43,7 @@ namespace SabberStoneCore.Model
             return Util.Choose<Card>(cards);
         }
 
-<<<<<<< HEAD
 		public string PickBasicTotem(string[] totems) => Util.Choose<string>(totems);
-=======
-		public virtual string PickBasicTotem(List<string> totems) => Util.Choose<string>(totems);
->>>>>>> test: add control tests
 
 		public virtual Card PickCard(RandomCardTask task, IEntity source, IEntity target, List<Card> cards) {
             return Util.Choose<Card>(cards);
@@ -63,11 +59,7 @@ namespace SabberStoneCore.Model
             return c.DeckZone.TopCard;
 		}
 
-<<<<<<< HEAD
 		public string PickEntourage(IEntity source, IEntity target, string[] cards)
-=======
-		public virtual string PickEntourage(IEntity source, IEntity target, List<string> cards)
->>>>>>> test: add control tests
 		{
 			return Util.Choose<string>(cards);
 		}
@@ -107,15 +99,11 @@ namespace SabberStoneCore.Model
 			return Util.Choose<Card>(cards);
 		}
 
-<<<<<<< HEAD
-        public Card PickSpell(IEntity source, IEntity target, Card[] cards) {
+        public virtual Card PickSpell(IEntity source, IEntity target, Card[] cards) {
             return Util.Choose<Card>(cards);
         }
 
 		public IPlayable PickTarget(EntityType type, IEntity source, IEntity target, List<IPlayable> entities) {
-=======
-		public virtual IPlayable PickTarget(EntityType type, IEntity source, IEntity target, List<IPlayable> entities) {
->>>>>>> test: add control tests
             return Util.Choose<IPlayable>(entities);
         }
 
@@ -151,11 +139,11 @@ namespace SabberStoneCore.Model
 	public class ThrowRandomController : IRandomController
 	{
 		public virtual Card PickAdaptChoices(EntityType type, IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickAdaptChoices not implemented");
-		public virtual string PickBasicTotem(List<string> totems) => throw new Exception("PickBasicTotem not implemented");
+        public virtual string PickBasicTotem(string[] totems) => throw new Exception("PickBasicTotem not implemented");
 		public virtual Card PickCard(RandomCardTask task, IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickCard not implemented");
 		public virtual Card PickDiscoverChoices(DiscoverType type, IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickDiscoverChoices not implemented");
 		public virtual IPlayable PickDraw(Controller c) => throw new Exception("PickDraw not implemented");
-		public virtual string PickEntourage(IEntity source, IEntity target, List<string> cards) => throw new Exception("PickEntourage not implemented");
+        public virtual string PickEntourage(IEntity source, IEntity target, string[] cards) => throw new Exception("PickEntourage not implemented");
 		public virtual CardClass PickHeroClass(IEntity source, IEntity target, CardClass[] classes) => throw new Exception("PickHeroClass not implemented");
 		public virtual IPlayable PickJoust(Controller c) => throw new Exception("PickJoust not implemented");
 		public virtual Card PickMinion(RandomMinionTask task, IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickMinion not implemented");
@@ -163,6 +151,7 @@ namespace SabberStoneCore.Model
 		public virtual Card PickPotionSpell(IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickPotionSpell not implemented");
 		public virtual Minion PickRecruit(IEntity source, IEntity target, List<Minion> minions) => throw new Exception("PickRecruit not implemented");
 		public virtual Card PickReplace(IZone zone, IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickReplace not implemented");
+        public virtual Card PickSpell(IEntity source, IEntity target, Card[] card) => throw new Exception("PickSpell not implemented");
 		public virtual IPlayable PickTarget(EntityType type, IEntity source, IEntity target, List<IPlayable> entities) => throw new Exception("PickTarget not implemented");
 		public virtual Card PickTransformMinion(IEntity source, IEntity target, List<Card> cards) => throw new Exception("PickTransformMinion not implemented");
 		public virtual string UnusedPickCardAsString(List<string> cards) => throw new Exception("UnusedPickCardAsString not implemented");
@@ -171,5 +160,5 @@ namespace SabberStoneCore.Model
 		public virtual int RandomDamage(int amount, IEntity source, IEntity target) => throw new Exception("RandomDamage not implemented");
 		public virtual int GetNumber(int min, int max, IEntity source, IEntity target) => throw new Exception("GetNumber not implemented");
 		public virtual List<IPlayable> SortSummonCopy(IEntity source, IEntity target, List<IPlayable> entities) => throw new Exception("SortSummonCopy not implemented");
-	}
+    }
 }
